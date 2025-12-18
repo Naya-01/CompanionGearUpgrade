61ff5411e6775614897f33d234b49fb1998ad223
Rayan <Rayan@DESKTOP-9I6JFB5>
2025-12-18 19:50:57 +0100

    Ajoutez des fichiers projet.

---
+++ SubModule.cs
@@ -0,0 +1,25 @@
+﻿using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace CompanionGearUpgrades
+{
+    public sealed class SubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            //if (game.GameType is not Campaign) return;
+
+            //if (gameStarterObject is CampaignGameStarter starter)
+            //{
+            //    starter.AddBehavior(new CompanionGearUpgradeBehavior());
+            //}
+
+            var starter = gameStarterObject as CampaignGameStarter;
+            if (starter == null)
+                return;
+
+            starter.AddBehavior(new CompanionGearUpgradeBehavior());
+        }
+    }
+}
